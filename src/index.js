c1e02cf6aaa290b8ecd8d41f23d500938541e514
LighthouseKeeperYN <freesaber@yandex.ru>
2019-09-26 22:01:37 +0300

    feat: task partially complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let factorialsInExpression = expression.split('*');
+
+  function zerosInEach(factorial) {
+    let factorialNumber = parseInt(factorial, 10);
+    let count = 0;
+    if (factorial.indexOf('!!') === -1) {
+      for (let i = 5; factorialNumber / i >= 1; i *= 5) {
+        count += factorialNumber / i;
+      }
+      return Math.floor(count);
+    }
+    else {
+      if (factorialNumber % 2 != 0) return 0;
+      for (let i = 10; factorialNumber / i >= 1; i *= 5) {
+        count += factorialNumber / i;
+      }
+      return Math.floor(count);
+    }
+  }
+
+  factorialsInExpression = factorialsInExpression.map(value => zerosInEach(value));
+ 
+  return factorialsInExpression.reduce((a, b) => a + b)
 }
